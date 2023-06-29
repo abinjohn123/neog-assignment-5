0a0d7ac6e9ca9f116fe0c89c81bf1f8ea85a8334
Abin John <abinjohnanto@gmail.com>
2023-06-29 11:15:17 +0530

    feat: post card

---
+++ src/Components/Home/Post.jsx
@@ -0,0 +1,54 @@
+import { HeartIcon, BookmarkIcon } from '../../icons/svg';
+
+const getName = (firstName = '', lastName = '') =>
+  `${firstName[0].toUpperCase()}${firstName.slice(
+    1
+  )} ${lastName[0].toUpperCase()}${lastName.slice(1)}`;
+
+const dateFormat = (dateObj) => {
+  return new Intl.DateTimeFormat('en-Us', {
+    hour: 'numeric',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  }).format(new Date(dateObj));
+};
+
+export const Post = ({ post, author }) => {
+  const {
+    content,
+    createdAt,
+    likes: { likeCount, likedBy, dislikedBy },
+  } = post;
+  const { firstName, lastName, username, avatar } = author;
+
+  return (
+    <div className="post-card">
+      <div className="header-container">
+        <div className="post-header">
+          <div className="avatar">
+            <img src={avatar ?? '/avatars/1.png'} alt={username} />
+          </div>
+          <div className="post-details">
+            <div className="author-details">
+              <p className="name">{getName(firstName, lastName)}</p>
+              <span className="username">@{username}</span>
+            </div>
+            <p className="creation">{dateFormat(createdAt)}</p>
+          </div>
+        </div>
+      </div>
+      <div className="post-content">
+        <p>{content}</p>
+      </div>
+      <div className="post-actions">
+        <div className="cur-p">
+          <HeartIcon />
+        </div>
+        <div className="cur-p">
+          <BookmarkIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,5 +1,34 @@
+import { useEffect } from 'react';
+
+import usePosts from '../../hooks/usePosts';
+import useUser from '../../hooks/useUser';
+import { Post } from './Post';
+
+import './feed.scss';
+
+/*
+  This is the main posts feed.
+  All posts will be displayed here
+*/
 const Home = () => {
-  return <h1>Home</h1>;
+  const { allPosts, isLoading: isPostsLoading, fetchPosts } = usePosts();
+  const { allUsers, isLoading: isUsersLoading, fetchUsers } = useUser();
+
+  useEffect(() => {
+    fetchPosts();
+    fetchUsers();
+  }, []);
+
+  if (isPostsLoading || allPosts.length === 0) return <p>Loading...</p>;
+  return (
+    <div className="feed">
+      {allPosts.map((post) => {
+        const postAuthor =
+          allUsers.find((user) => user.username === post.username) ?? {};
+        return <Post post={post} key={post._id} author={postAuthor} />;
+      })}
+    </div>
+  );
 };
 
 export default Home;

4ac897d4ca2ced56c6b7fc42822c9e6d05530c5e
Abin John <abinjohnanto@gmail.com>
2023-06-29 14:47:30 +0530

    style: layout changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/mockman" element={<Mockman colorScheme="dark" />} />
       </Routes>
-      <Sidebar />
     </>
   );
 }
--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import usePosts from '../../hooks/usePosts';
 import useUser from '../../hooks/useUser';
 import { Post } from './Post';
+import Sidebar from '../Sidebar';
 
 import './feed.scss';
 
@@ -54,30 +55,33 @@ const Home = () => {
 
   if (isPostsLoading || allPosts.length === 0) return <p>Loading...</p>;
   return (
-    <div className="feed">
-      <div className="sort-card">
-        <h3>Posts</h3>
-        <div className="sort-controls">
-          <button
-            onClick={() => setSort(SORT_KEYS.TRENDING)}
-            className={sort === SORT_KEYS.TRENDING ? 'active' : ''}
-          >
-            Trending
-          </button>
-          <button
-            onClick={() => setSort(SORT_KEYS.LATEST)}
-            className={sort === SORT_KEYS.LATEST ? 'active' : ''}
-          >
-            Latest
-          </button>
+    <>
+      <div className="feed">
+        <div className="sort-card">
+          <h3>Posts</h3>
+          <div className="sort-controls">
+            <button
+              onClick={() => setSort(SORT_KEYS.TRENDING)}
+              className={sort === SORT_KEYS.TRENDING ? 'active' : ''}
+            >
+              Trending
+            </button>
+            <button
+              onClick={() => setSort(SORT_KEYS.LATEST)}
+              className={sort === SORT_KEYS.LATEST ? 'active' : ''}
+            >
+              Latest
+            </button>
+          </div>
         </div>
+        {allPosts.map((post) => {
+          const postAuthor =
+            allUsers.find((user) => user.username === post.username) ?? {};
+          return <Post post={post} key={post._id} author={postAuthor} />;
+        })}
       </div>
-      {allPosts.map((post) => {
-        const postAuthor =
-          allUsers.find((user) => user.username === post.username) ?? {};
-        return <Post post={post} key={post._id} author={postAuthor} />;
-      })}
-    </div>
+      <Sidebar />
+    </>
   );
 };
 

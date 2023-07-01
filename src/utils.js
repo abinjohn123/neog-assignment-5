63417e47455cbe221b226b9e7a714a244086ffa3
Abin John <abinjohnanto@gmail.com>
2023-07-01 10:19:55 +0530

    refactor: firstName util function

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,6 @@ export const getFullName = (firstName = '', lastName = '') =>
   `${firstName[0].toUpperCase()}${firstName.slice(
     1
   )} ${lastName[0].toUpperCase()}${lastName.slice(1)}`;
+
+export const getFirstName = (firstName = '') =>
+  `${firstName[0].toUpperCase()}${firstName.slice(1)}`;

f86d7473ac9147f6ad2a976b738792b76f4b1c08
Abin John <abinjohnanto@gmail.com>
2023-07-01 10:20:47 +0530

    feat: show user posts in profile page

---
+++ src/Components/Profile/Feed.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+
+import { useAuthContext } from '../../context/AuthContext';
+import usePosts from '../../hooks/usePosts';
+import { getFirstName } from '../../utils';
+
+import { Post } from '../Home/Post';
+import Sidebar from '../Sidebar';
+
+export const ProfileFeed = ({ profile }) => {
+  const { username = '' } = useAuthContext();
+  const { allPosts, fetchPosts } = usePosts();
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className="profile-feed">
+      <h3 className="profile-h3">
+        {profile.username === username
+          ? 'Your posts'
+          : `${getFirstName(profile.firstName)}'s posts`}
+      </h3>
+      <div className="layout">
+        <div>
+          {allPosts
+            .filter((post) => post.username === profile.username)
+            .map((post) => {
+              return <Post post={post} key={post._id} author={profile} />;
+            })}
+        </div>
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
--- src/Components/Profile/index.jsx
+++ src/Components/Profile/index.jsx
@@ -4,8 +4,9 @@ import useUser from '../../hooks/useUser';
 import { useEffect } from 'react';
 
 import { getFullName } from '../../utils';
-import './profile.scss';
 import { GlobeIcon } from '../../icons/svg';
+import { ProfileFeed } from './Feed';
+import './profile.scss';
 
 const Profile = () => {
   const { userId } = useParams();
@@ -18,19 +19,22 @@ const Profile = () => {
   if (isLoading) return <p>Loading...</p>;
 
   return (
-    <div className="profile-card">
-      <div className="img-card">
-        <img src={user.avatar} username={user.username} />
-      </div>
+    <>
+      <div className="profile-card">
+        <div className="img-card">
+          <img src={user.avatar} username={user.username} />
+        </div>
 
-      <div className="container">
-        <p className="name">{getFullName(user.firstName, user.lastName)}</p>
-        <Link to={user.link ?? '/'} className="web-url">
-          <GlobeIcon />
-        </Link>
+        <div className="container">
+          <p className="name">{getFullName(user.firstName, user.lastName)}</p>
+          <Link to={user.link ?? '/'} className="web-url">
+            <GlobeIcon />
+          </Link>
+        </div>
+        <p className="bio">{user.bio ?? 'A catchy bio goes here'}</p>
       </div>
-      <p className="bio">{user.bio ?? 'A catchy bio goes here'}</p>
-    </div>
+      <ProfileFeed profile={user} />
+    </>
   );
 };
 

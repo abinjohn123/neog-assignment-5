a3330dbca843d8d20d4726fb85e5018e8e5e7caf
Abin John <abinjohnanto@gmail.com>
2023-07-08 15:14:00 +0530

    feat: spinner button

---
+++ src/Components/Shared/SpinnerButton.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef } from 'react';
+
+import { noop } from '../../utils';
+
+const SpinnerButton = ({
+  children,
+  type = 'button',
+  className,
+  isLoading = false,
+  disabled = false,
+  onClick = noop,
+}) => {
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    if (isLoading) contentRef.current.style.visibility = 'hidden';
+    else contentRef.current.style.visibility = 'visible';
+  }, [isLoading]);
+
+  return (
+    <button
+      className={`btn spinner-btn ${className} ${
+        disabled || isLoading ? 'disabled' : ''
+      }`}
+      type={type}
+      disabled={disabled || isLoading}
+      onClick={onClick}
+    >
+      {isLoading && (
+        <div className="spinner-container">
+          <div className="spinner"></div>
+        </div>
+      )}
+      <div className="content" ref={contentRef}>
+        {children}
+      </div>
+    </button>
+  );
+};
+
+export default SpinnerButton;
